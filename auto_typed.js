5c8fac9c2d9d6e9af8e51fd5efb67b156ac8c04d
Prashant Rana <ranaprashant282@gmail.com>
2025-02-19 23:16:22 +0530

    Fourth Commit

---
+++ auto_typed.js
@@ -0,0 +1,9 @@
+const typed = new Typed(".auto-type", {
+  strings: ["Developer", "Designer", "Freelancer", "Car Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+  backDelay: 1000,
+  cursorChar: "|",
+  smartBackspace: true,
+});
